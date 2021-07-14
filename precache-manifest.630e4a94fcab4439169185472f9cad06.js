047bc126a2d5ec12255f4a2752efcd6ca0dbe269
H4mb01 <manuel@muenstedt.de>
2021-07-14 14:13:26 +0200

    first commit

---
+++ precache-manifest.630e4a94fcab4439169185472f9cad06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44d13207910a3978adc",
+    "url": "/css/app.d5255d2f.css"
+  },
+  {
+    "revision": "032956d2503e7d10ae2c717080044114",
+    "url": "/img/seamlessWood.032956d2.jpg"
+  },
+  {
+    "revision": "cef4e10a781d3045c02f5cd65e038b58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34de357d88170405dc9e",
+    "url": "/js/about.258e4cc5.js"
+  },
+  {
+    "revision": "f44d13207910a3978adc",
+    "url": "/js/app.633b212f.js"
+  },
+  {
+    "revision": "55bbef34c9e4efa275fc",
+    "url": "/js/chunk-vendors.97d64dc2.js"
+  },
+  {
+    "revision": "6a15c9db6dbe7831ca2a27db12fb5685",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
